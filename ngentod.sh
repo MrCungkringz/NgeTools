ee21ec184adc0669fc53e3cbd03923b66a8c7e6f
MrCungkringz <45120964+MrCungkringz@users.noreply.github.com>
2018-11-17 18:26:50 +0700

    Add files via upload

---
+++ ngentod.sh
@@ -0,0 +1,56 @@
+# usr/bin/bash
+echo
+echo "==============================" | lolcat
+echo "Pembuat  = Mr. Cungkringz" | lolcat
+echo "Kontak   = 08996226529" | lolcat
+echo "Email    = ramdanikuncoro@gmail" | lolcat
+echo "Grup     = Belajar Termux" | lolcat
+echo "==============================" | lolcat
+echo "Jadikanlah Ini Untuk Keperluan" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>>>" | lolcat
+echo " (1)Sadap Wa Ampuh" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>>>" | lolcat
+echo " (2)HackWifi No root" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>>>" | lolcat
+echo " (3)Buat Virus Wa" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>>>" | lolcat
+echo " (4)Lacak Imei" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>>>" | lolcat
+
+read -p "Cungkringz =" user
+
+if [ $user = 1 ] || [ $user = 1 ]
+then
+
+
+echo "MAMPOES KETIPU KAMU BANGSAT" | lolcat
+echo    "MAMPOES WKWKWK KONTOL" | lolcat
+sh langka.sh
+fi
+
+if [ $user = 2 ] || [ $user = 2 ]
+then
+
+
+echo "MAMPOES KETIPU KAMU BANGSAT" | lolcat
+echo    "MAMPOES WKWKWK KONTOL" | lolcat
+sh langka.sh
+fi
+
+if [ $user = 3 ] || [ $user = 3 ]
+then
+
+
+echo "MAMPOES KETIPU KAMU BANGSAT" | lolcat
+echo    "MAMPOES WKWKWK KONTOL" | lolcat
+sh langka.sh
+fi
+
+if [ $user = 4 ] || [ $user = 4 ]
+then
+
+
+echo "MAMPOES KETIPU KAMU BANGSAT" | lolcat
+echo    "MAMPOES WKWKWK KONTOL" | lolcat
+sh langka.sh
+fi
